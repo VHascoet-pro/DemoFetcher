c14c0051c66ba832d413e195f30883775399be14
VHascoet-pro <hascoet.vincent.pro@gmail.com>
2026-02-25 19:38:11 +0100

    Cleaner code for now

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/directory.hpp>
-#include <boost/filesystem/path.hpp>
+#include <boost/filesystem.hpp>
 #include <iostream>
-
-bool FileChecker(boost::filesystem::path inputPath, boost::filesystem::path demoPath, const std::string exList[]){
+using namespace boost::filesystem; 
+bool FileChecker(path inputPath, path demoPath, const std::string exList[]){
   int i = 0;
   bool validFile;
   while(inputPath.extension() != exList[i] && (i>=0 && i <= 3)){
@@ -15,29 +13,30 @@ bool FileChecker(boost::filesystem::path inputPath, boost::filesystem::path demo
   }
 
   if(validFile == true){
-    boost::filesystem::copy_file(inputPath, demoPath);
+    copy_file(inputPath, demoPath);
     return validFile;
   } else return validFile;
 }
 
-void RecursiveCheck(boost::filesystem::path inputPath, const std::string exList[]){
-  for(boost::filesystem::recursive_directory_iterator end; inputPath != end; ++inputPath){} // Prototype, I don't know how to continue this shit'
+void RecursiveCheck(path inputPath, const std::string exList[]){
+  directory_iterator dir;
+  for(recursive_directory_iterator end; dir != end; ++dir){} // Prototype, I don't know how to continue this shit'
 }
 
 int main(int argc, char* argv[1]){
   const std::string extensionList[] = {".replay.Gbx", ".dem", ".lsp", ".SC2Replay"};
 
-  boost::filesystem::path Path;
-  boost::filesystem::path demoPath = "./demos/";
+  path Path;
+  path demoPath = "./demos/";
 
   std::cout<<"Verifying the existence of the demo folder...";
-  if(!boost::filesystem::exists(demoPath)){
-    boost::filesystem::create_directory(demoPath);
+  if(!exists(demoPath)){
+    create_directory(demoPath);
   }
 
   std::cout<<"Enter a path to recursively search on : ";
   std::cin>>Path;
-  while(!(boost::filesystem::exists(Path))){
+  while(!(exists(Path))){
     std::cout<<"This path does not exist, please enter an existing path : ";
     std::cin>>Path;
   }
