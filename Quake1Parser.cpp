1073e085540a85bba17641b1520db90ccebeb7f2
VHascoet-pro <hascoet.vincent.pro@gmail.com>
2026-03-18 11:11:01 +0100

    MOAR !!!

--- Quake1Parser.cpp
+++ Quake1Parser.cpp
@@ -1,26 +1,49 @@
 #include "fileCopy.h"
-#include <array>
 #include <boost/filesystem.hpp>
-#include <cstdint>
 #include <fstream>
-#include <vector>
 
 DemoParsers::GAME_PATHS gp;
 
-struct Quake1Demo_MAP{
-        // Inclus tous les messages réseau envoyés dans le fichier .dem
-        long SVC_TIMEBLOCK;
-        char* SVC_INTERMISSION;
-        bool SVC_isCOMPLETED;
+/**
+ Ces deux structures sont pour avoir :
+ 1) Les blocks à ne PAS skipper triés par Identification
+ 2) Les types des blocks suivant l'identification (EX: pour SVC_TIME : 27.13s, on à 0x07 F5D8D941 > trié en Little-endian : 41D9D8F5 > calcul suivant IEEE :
+                                                                       1x 2^131-127 x (1+ 5878261 / 2^23) = 27.21189308s)
+*/
+struct QUAKE_BLOCKS{
+        int SVC_TIME = 0x07;
+        int SVC_PLAYERNAME = 0x0D;
+        int SVC_INTERMISSION = 0x1E;
+        int SVC_DISCONNECTMESSAGE = 0x02;
 };
+
+struct QUAKE_BLOCKTYPES{
+        long SVC_TIMESIZE;
+        char SVC_PLAYER;
+        char SVC_IntMessage;
+        bool DISCONNECTED;
+};
+
+/**
+ On à pas besoin de beaucoup de trames afin d'analyser les trames, analyser toutes les trames
+ serait trop long et demandera trop de mémoire, on doit SKIPPER des trames.
+
+ @param isSkipped Retourne l'état du Parseur (est bel et bien évité).
+*/
+
+bool BlockSkip(){
+        bool isSkipped = false;
+        return isSkipped;
+}
+
 /**
  Cette fonction permet de parser les données du fichier de démo et en sortir le niveau + temps
  pour pouvoir l'inclure dans le fichier JSON nécessaire
  @param q1DemoPath Chemin absolu du fichier de démo envoyé depuis la recherche récursive de la
                    fonction FileCopy
  
- @param Quake1Demo_MAP Une structure me permettant d'y inclure les types de messages
-                       rencoyés depuis le parseur
+ @param QUAKE_BLOCKS Une structure me permettant d'y inclure les types de messages
+                     renvoyés depuis le parseur
 
  @param SVC_MESSAGE Message contenant le temps (1 unité par tic de 10Hz) de la démo, le nom de
                     la map, joueur, angle de la caméra du joueur (x-pitch, y-roll, z-yaw) par
@@ -31,6 +54,5 @@ struct Quake1Demo_MAP{
 
 void DemoParsers::DemoOperator::Quake1DemoToJson(boost::filesystem::path q1DemoPath){
         std::ifstream LocalDemo(q1DemoPath, std::ios_base::in | std::ios_base::binary);
-
-        while(LocalDemo.is_open()){}
+        
 }
